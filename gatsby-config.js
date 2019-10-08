432857d2e35ad7f0de6a575b83b5e05d40c642b9
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-10-08 16:11:37 -0600

    Configure for PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ const siteMetadata = {
   siteUrl: "https://atombuild.github.io",
   description: "Integrate your Build System with Atom.",
   color: {
-    primary: "efeae1",
-    secondary: "66595c",
-    active: "81c08b"
+    primary: "#efeae1",
+    secondary: "#66595c",
+    active: "#81c08b"
   },
   nav: [
     { name: "Atom Package Manager", url: "https://atom.io/packages/build" },
@@ -16,5 +16,20 @@ const siteMetadata = {
 
 module.exports = {
   siteMetadata,
-  plugins: ["gatsby-theme-atom-service"]
+  plugins: [
+    "gatsby-theme-atom-service",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: siteMetadata.title,
+        short_name: siteMetadata.title,
+        start_url: "/",
+        background_color: siteMetadata.color.secondary,
+        theme_color: siteMetadata.color.primary,
+        display: "minimal-ui",
+        icon: "content/assets/logo.svg"
+      }
+    },
+    "gatsby-plugin-offline"
+  ]
 };
